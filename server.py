91403e57242655f006ecd7160325bafd7c4b45ec
Epafroditus George <cavidjaja@gmail.com>
2021-07-16 13:48:55 +0700

    first commit

---
+++ server.py
@@ -0,0 +1,56 @@
+import socket
+import threading
+
+HOST = '127.0.0.1'
+PORT = 9090
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+
+server.listen()
+
+clients = []
+nicknames = []
+
+
+# brodcast function
+def brodcast(message):
+    for client in clients:
+        client.send(message)
+
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            print(f'{nicknames[client.index(client)]} says {message}')
+            brodcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            break
+
+# receive function
+
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f'Connected with {str(address)}!')
+        client.send("NICK".encode('utf-8'))
+        nickname = client.recv(1024)
+
+        nicknames.append(nickname)
+        clients.append(client)
+        print(f'Nickaname of the client is {nickname}')
+        brodcast(f'{nickname} connected to the server!\n'.encode('utf-8'))
+        client.send("Connected to the server".encode('utf-8'))
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+print('Server running...')
+receive()
